d7497088da5e3ecdf9dfade2671cd03fe211fa3a
Docz <mister-dan-zubkov@mail.ru>
2025-09-01 14:34:54 +0500

    update

---
+++ main/variables.tf
@@ -0,0 +1,8 @@
+variable "cloud_id" {
+  type    = string
+  default = "b1gnh4gft3n8r401os0m"
+}
+variable "folder_id" {
+  type    = string
+  default = "b1gppvvk1mpl2kum0b2m"
+}
---
+++ main/vms.tf
@@ -0,0 +1,312 @@
+
+#считываем данные об образе ОС
+data "yandex_compute_image" "ubuntu_2204_lts" {
+  family = "ubuntu-2204-lts"
+}
+
+/// Bastion
+resource "yandex_compute_instance" "bastion" {
+  name        = "bastion" #Имя ВМ в облачной консоли
+  hostname    = "bastion" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a" #зона ВМ должна совпадать с зоной subnet!!!
+
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_2204_lts.image_id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+  
+  lifecycle{
+    ignore_changes = [boot_disk.0.initialize_params.0.image_id]
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = 1
+  }
+
+  scheduling_policy { preemptible = true }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop_a.id #зона ВМ должна совпадать с зоной subnet!!!
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.LAN.id, yandex_vpc_security_group.bastion.id]
+  }
+}
+
+/// Web-A
+resource "yandex_compute_instance" "web_a" {
+  name        = "web-a" #Имя ВМ в облачной консоли
+  hostname    = "web-a" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a" #зона ВМ должна совпадать с зоной subnet!!!
+
+
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_2204_lts.image_id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  lifecycle{
+    ignore_changes = [boot_disk.0.initialize_params.0.image_id]
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = 1
+  }
+
+  scheduling_policy { preemptible = true }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop_a.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.LAN.id, yandex_vpc_security_group.web_sg.id]
+  }
+}
+
+/// Web-B
+resource "yandex_compute_instance" "web_b" {
+  name        = "web-b" #Имя ВМ в облачной консоли
+  hostname    = "web-b" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
+  platform_id = "standard-v3"
+  zone        = "ru-central1-b" #зона ВМ должна совпадать с зоной subnet!!!
+
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_2204_lts.image_id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  lifecycle{
+    ignore_changes = [boot_disk.0.initialize_params.0.image_id]
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = 1
+  }
+
+  scheduling_policy { preemptible = true }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop_b.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.LAN.id, yandex_vpc_security_group.web_sg.id]
+
+  }
+}
+
+/// Zabbix_server
+resource "yandex_compute_instance" "zabbix" {
+  name        = "zabbix" #Имя ВМ в облачной консоли
+  hostname    = "zabbix" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a" #зона ВМ должна совпадать с зоной subnet!!!
+
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_2204_lts.image_id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  lifecycle{
+    ignore_changes = [boot_disk.0.initialize_params.0.image_id]
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = 1
+  }
+
+  scheduling_policy { preemptible = true }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop_a_pub.id
+    nat                = true
+    /// security_group_ids = [yandex_vpc_security_group.LAN.id, yandex_vpc_security_group.web_sg.id]
+
+  }
+}
+
+
+/// Elasticsearch
+resource "yandex_compute_instance" "elastic" {
+  name        = "elastic" #Имя ВМ в облачной консоли
+  hostname    = "elastic" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a" #зона ВМ должна совпадать с зоной subnet!!!
+
+  resources {
+    cores         = 4
+    memory        = 8
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_2204_lts.image_id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  lifecycle{
+    ignore_changes = [boot_disk.0.initialize_params.0.image_id]
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = 1
+  }
+
+  scheduling_policy { preemptible = true }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop_a.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.LAN.id, yandex_vpc_security_group.web_sg.id]
+
+  }
+}
+
+
+/// Logstash
+resource "yandex_compute_instance" "logstash" {
+  name        = "logstash" #Имя ВМ в облачной консоли
+  hostname    = "logstash" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a" #зона ВМ должна совпадать с зоной subnet!!!
+
+  resources {
+    cores         = 4
+    memory        = 8
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_2204_lts.image_id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  lifecycle{
+    ignore_changes = [boot_disk.0.initialize_params.0.image_id]
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = 1
+  }
+
+  scheduling_policy { preemptible = true }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop_a.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.LAN.id, yandex_vpc_security_group.web_sg.id]
+
+  }
+}
+
+
+/// Kibana
+resource "yandex_compute_instance" "kibana" {
+  name        = "kibana" #Имя ВМ в облачной консоли
+  hostname    = "kibana" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a" #зона ВМ должна совпадать с зоной subnet!!!
+
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_2204_lts.image_id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  lifecycle{
+    ignore_changes = [boot_disk.0.initialize_params.0.image_id]
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = 1
+  }
+
+  scheduling_policy { preemptible = true }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop_a_pub.id
+    nat                = true
+    /// security_group_ids = [yandex_vpc_security_group.LAN.id, yandex_vpc_security_group.web_sg.id]
+
+  }
+}
+
+/// Inventory for Ansible
+resource "local_file" "inventory" {
+  content  = <<-XYZ
+  [bastion]
+  ${yandex_compute_instance.bastion.hostname}.ru-central1.internal
+
+  [webservers]
+  ${yandex_compute_instance.web_a.hostname}.ru-central1.internal
+  ${yandex_compute_instance.web_b.hostname}.ru-central1.internal
+  
+  [zabbix]
+  ${yandex_compute_instance.zabbix.hostname}.ru-central1.internal
+
+  [elasticsearch]
+  ${yandex_compute_instance.elastic.hostname}.ru-central1.internal
+
+  [logstash]
+  ${yandex_compute_instance.logstash.hostname}.ru-central1.internal
+
+  [kibana]
+  ${yandex_compute_instance.kibana.hostname}.ru-central1.internal
+  XYZ
+  filename = "./ansible/inventory.yml"
+}
